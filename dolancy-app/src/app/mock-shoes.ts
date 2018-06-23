31cb13e04699136dad570c2c2f5fd5c4112a9f3d
RegulEsteban <regulesteban@gmail.com>
2018-06-22 23:23:37 -0500

    avances a la galería y a los detalles

--- dolancy-app/src/app/mock-shoes.ts
+++ dolancy-app/src/app/mock-shoes.ts
@@ -1,14 +1,33 @@
 import { Shoes } from './shoes';
 
 export const SHOES: Shoes[] = [
-  { id: 11, color: 'Mr. Nice' },
-  { id: 12, color: 'Narco' },
-  { id: 13, color: 'Bombasto' },
-  { id: 14, color: 'Celeritas' },
-  { id: 15, color: 'Magneta' },
-  { id: 16, color: 'RubberMan' },
-  { id: 17, color: 'Dynama' },
-  { id: 18, color: 'Dr IQ' },
-  { id: 19, color: 'Magma' },
-  { id: 20, color: 'Tornado' }
+  { 
+    id: 1, 
+    color: 'Mr. Nice',
+    modelo: '221 Miel',
+    album: '/assets/images/catalogo/221-miel/',
+    detail: ['1.jpg', '2.jpg', '3.jpg'],
+    description: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo. xxx',
+    price: 455.00,
+    properties: {
+        piel: 'Ternera',
+        forro: 'Flor de cerdo',
+        suela: '100% cuero',
+        tallas: '25 al 29'
+    }
+  },{ 
+    id: 2, 
+    color: 'Narco',
+    modelo: '097 Negro',
+    album: '/assets/images/catalogo/097-negro/',
+    detail: ['1.jpg', '2.jpg', '3.jpg'],
+    description: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.',
+    price: 455.00,
+    properties: {
+        piel: 'Ternera',
+        forro: 'Flor de cerdo',
+        suela: '100% cuero',
+        tallas: '25 al 29'
+    }
+  }
 ];
--- dolancy-app/src/app/shoes.ts
+++ dolancy-app/src/app/shoes.ts
@@ -1,4 +1,15 @@
 export class Shoes {
     id: number;
     color: string;
+    modelo: string;
+    album: string;
+    description: string;
+    detail: string[];
+    price: number;
+    properties: {
+        piel: string;
+        forro: string;
+        suela: string;
+        tallas: string;
+    };
 }
