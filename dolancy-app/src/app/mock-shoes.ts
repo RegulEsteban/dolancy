0d094af579750ee8e764c4891530326b60e5da97
Nancyre <regulesnancy253@gmail.com>
2018-06-23 00:58:07 -0500

    Descripción

--- dolancy-app/src/app/mock-shoes.ts
+++ dolancy-app/src/app/mock-shoes.ts
@@ -28,6 +28,19 @@ export const SHOES: Shoes[] = [
         forro: 'Flor de cerdo',
         suela: '100% cuero',
         tallas: '25 al 29'
+    },{ 
+    id: 3, 
+    color: 'Narco',
+    modelo: '097 Negro',
+    album: '/assets/images/catalogo/097-negro/',
+    detail: ['1.jpg', '2.jpg', '3.jpg'],
+    description: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.',
+    price: 455.00,
+    properties: {
+        piel: 'Ternera',
+        forro: 'Flor de cerdo',
+        suela: '100% cuero',
+        tallas: '25 al 29'
     }
   }
 ];
